ebef2a5023e0a14e8cf0ce790d1540e9e036b3d3
Qweastik <qweastik@gmail.com>
2023-05-23 18:00:52 +0300

    Commit 9

---
+++ Assets/scripts/Character/Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void  takeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if(currentHealth > 0)
+        {
+
+        }
+
+        else
+        {
+
+        }
+    }
+}
